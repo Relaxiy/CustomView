4fbadccb40a37c23366b17e5025ebf6cfd62ae72
Relaxiy <terer435461@gmail.com>
2022-04-28 14:52:55 +0300

    CustomView

---
+++ app/src/main/java/com/example/customview/CustomView.kt
@@ -0,0 +1,94 @@
+package com.example.customview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Paint.ANTI_ALIAS_FLAG
+import android.graphics.Paint.Style
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+
+class CustomView(
+    context: Context,
+    attrs: AttributeSet
+) : View(context, attrs)  {
+
+    companion object {
+        private const val LINE_WIDTH = 10f
+        private const val LETTERS_LINE_WIDTH = 100f
+        private const val START_COORDINATE = 0f
+        private const val START_ANGLE = 102f
+        private const val SWEEP_ANGLE = 156f
+    }
+
+    init {
+        context.theme.obtainStyledAttributes(attrs, R.styleable.CustomView, 0, 0)
+            .apply {
+                customText = getString(R.styleable.CustomView_CustomText).toString()
+            }
+    }
+
+    private var customText: String
+
+    private val textPaint = Paint(ANTI_ALIAS_FLAG)
+        .apply {
+            color = context.getColor(R.color.white)
+            strokeWidth = LETTERS_LINE_WIDTH
+        }
+
+    private val backgroundPaint = Paint(ANTI_ALIAS_FLAG)
+        .apply {
+            strokeWidth = LINE_WIDTH
+            style = Style.STROKE
+            color = context.getColor(R.color.black)
+        }
+
+    private val rectangleBackgroundPaint = Paint(ANTI_ALIAS_FLAG)
+        .apply {
+            color = context.getColor(R.color.black)
+        }
+
+    private val centerOfX: Float get() = (width / 2).toFloat()
+
+    private var defaultStartX = 0f
+    private var stopX = 0f
+    private var textCoordinateX = 0f
+    private var textCoordinateY = 0f
+
+    private val arcOval by lazy {
+        RectF(
+            centerOfX,
+            17.5f,
+            centerOfX + width - 90f,
+            height.toFloat() - 17.5f
+        )
+    }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+        textPaint.textSize = height/3.5f
+        defaultStartX = width/1.15f
+        stopX = width.toFloat() - 15f
+        textCoordinateX = centerOfX - textPaint.measureText(customText) / 1.8f
+        textCoordinateY = height/1.7f
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        canvas.drawLine(defaultStartX, height.toFloat(), stopX, height/2f, backgroundPaint)
+        canvas.drawLine(defaultStartX, START_COORDINATE, stopX, height/2f, backgroundPaint)
+        canvas.drawLine(START_COORDINATE, height.toFloat(), defaultStartX, height.toFloat(), backgroundPaint)
+        canvas.drawLine(START_COORDINATE, START_COORDINATE, defaultStartX, START_COORDINATE, backgroundPaint)
+        canvas.drawLine(START_COORDINATE, START_COORDINATE, START_COORDINATE, height.toFloat(), backgroundPaint)
+        canvas.drawArc(arcOval, START_ANGLE, SWEEP_ANGLE, true, rectangleBackgroundPaint)
+        canvas.drawRect(5f,20f, defaultStartX - 20f, height - 20f, rectangleBackgroundPaint)
+        canvas.drawText(
+            customText,
+            textCoordinateX,
+            textCoordinateY,
+            textPaint
+        )
+    }
+}
